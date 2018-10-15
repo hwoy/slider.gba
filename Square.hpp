e780725a247e51ad5436d95c8602304030069970
Watthanachai Dueanklang <bosskillerz@gmail.com>
2018-10-15 21:16:53 +0700

    new file:   Square.hpp 	modified:   main.cpp

---
+++ Square.hpp
@@ -0,0 +1,32 @@
+#ifndef __SQUARE_HPP__
+#define __SQUARE_HPP__
+
+#include "arm7type.hpp"
+#include "Graphic.hpp"
+#include "Number.hpp"
+
+
+struct Square
+{
+	u8arm_t width,iwidth;
+	Color color,icolor,ncolor;
+
+	void print(Graphic &g,const Point &point,u8arm_t num) const
+	{
+		g.rectangle(color,point.x,point.y,point.x+width-1,point.y+width-1);
+
+		u8arm_t ix=point.x+(width-iwidth)/2;
+		u8arm_t iy=point.y+(width-iwidth)/2;
+
+		g.rectangle(icolor,ix,iy,ix+iwidth-1,iy+iwidth-1);
+
+		u8arm_t nx=ix+(iwidth-Number::SIZE)/2;
+		u8arm_t ny=iy+(iwidth-Number::SIZE)/2;
+
+		Number::print(g,{nx,ny},ncolor,num);
+	}
+};
+
+
+#endif
+
--- main.cpp
+++ main.cpp
@@ -2,17 +2,31 @@
 #include "Graphic.hpp"
 #include "Keypad.hpp"
 #include "Number.hpp"
+#include "Square.hpp"
+
+#define WIDTH 50
+#define IWIDTH 20
+
+#define CGAP ((GraphicDevice::COL-(WIDTH*3))/4)
+#define RGAP ((GraphicDevice::ROW-(WIDTH*3))/4)
+
+static constexpr const Color BOXCOLOR  {31,0,0};
+static constexpr const Color IBOXCOLOR {0,31,0};
+static constexpr const Color NUMCOLOR  {0,0,31};
+
+static constexpr const Color BGCOLOR   {0,0,0};
 
 extern "C"
 int main()
 {
 	Graphic g(GraphicDevice{});
-	g.setbgcolor(Color::rgb(0,0,31));
-	
-	Color color{31,0,0};
+	g.setbgcolor(BGCOLOR);
 
-	for(u8arm_t i=0;i<10;++i)
-		Number::print(g,{0,static_cast<u8arm_t>(i*8)},color,i);
+	const Square square{WIDTH,IWIDTH,BOXCOLOR,IBOXCOLOR,NUMCOLOR};
+
+	for(u8arm_t i=0,rgap=RGAP,num=1;i<3;++i,rgap+=(RGAP+square.width))
+		for(u8arm_t j=0,cgap=CGAP;j<3;++j,cgap+=(CGAP+square.width),++num)
+			square.print(g,{cgap,rgap},num);
 
 	return 0;
 }
