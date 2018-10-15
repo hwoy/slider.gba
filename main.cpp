de4655ba5875fb11288f940aab79b36d40a19f7b
Watthanachai Dueanklang <bosskillerz@gmail.com>
2018-10-15 21:26:09 +0700

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@
 #define WIDTH 50
 #define IWIDTH 20
 
-#define CGAP ((GraphicDevice::COL-(WIDTH*3))/4)
 #define RGAP ((GraphicDevice::ROW-(WIDTH*3))/4)
+#define CGAP RGAP
+
+#define FCGAP  ((GraphicDevice::COL-(CGAP*2)-(WIDTH*3))/2)
+#define FRGAP  ((GraphicDevice::ROW-(RGAP*2)-(WIDTH*3))/2)
 
 static constexpr const Color BOXCOLOR  {31,0,0};
 static constexpr const Color IBOXCOLOR {0,31,0};
@@ -24,8 +27,8 @@ int main()
 
 	const Square square{WIDTH,IWIDTH,BOXCOLOR,IBOXCOLOR,NUMCOLOR};
 
-	for(u8arm_t i=0,rgap=RGAP,num=1;i<3;++i,rgap+=(RGAP+square.width))
-		for(u8arm_t j=0,cgap=CGAP;j<3;++j,cgap+=(CGAP+square.width),++num)
+	for(u8arm_t i=0,rgap=FRGAP,num=1;i<3;++i,rgap+=(RGAP+square.width))
+		for(u8arm_t j=0,cgap=FCGAP;j<3;++j,cgap+=(CGAP+square.width),++num)
 			square.print(g,{cgap,rgap},num);
 
 	return 0;
