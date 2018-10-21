5619d11b46ff57a222d196810472486e5a115a82
Watthanachai Dueanklang <bosskillerz@gmail.com>
2018-10-22 01:25:28 +0700

    modified:   Draw.hpp

--- Draw.hpp
+++ Draw.hpp
@@ -6,11 +6,11 @@
 
 struct Draw 
 {
-	template <usize_t M,usize_t N>
-	static void draw(Graphic &g,const Point &point,const Color &color,const u8arm_t (&alpha)[M][N][N],const u8arm_t ch)
+	template <usize_t M,usize_t N,usize_t P>
+	static void draw(Graphic &g,const Point &point,const Color &color,const u8arm_t (&alpha)[M][N][P],const u8arm_t ch)
 	{
 		for(u8arm_t y=0;y<N;++y)
-			for(u8arm_t x=0;x<N;++x)
+			for(u8arm_t x=0;x<P;++x)
 				if(alpha[ch][y][x])
 					g.pixel(color,x+point.x,y+point.y);
 	}
