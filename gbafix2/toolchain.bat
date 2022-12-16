1573610722e1b432ad77ba63ef3c760480a02c8d
Watthanachai Dueanklang <bosskillerz@gmail.com>
2022-12-16 17:50:51 +0700

    modified:   gbafix2/.gitignore 	modified:   gbafix2/main.c 	deleted:    gbafix2/toolchain.bat

--- gbafix2/toolchain.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PATH=C:\Strawberry\perl\bin;C:\Python27;C:\mingw64-toolchain\bin;C:\llvm\bin;C:\cmake-3.12.2-win64-x64\bin;C:\Git\bin;%PATH%;C:\tool
-cmd.exe /K title MinGw64-toolchain
